6ec4d0a95ef7f3a513427f0d74b0629c06317d16
tjdrkr2580 <tjdrkr2580@naver.com>
2023-02-02 08:08:28 +0900

    ADD | Tailwind Error fix

--- fe/src/pages/_app.tsx
+++ fe/src/pages/_app.tsx
@@ -1,10 +1,10 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { MantineProvider } from '@mantine/core'
 import React, { useEffect } from 'react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import axios from 'axios'
+import '@/styles/globals.css'
 
 const queryClient = new QueryClient({
   defaultOptions: {
