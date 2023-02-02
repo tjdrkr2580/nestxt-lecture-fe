92b86efadb76996eaa5c7e84a202371b690bb929
tjdrkr2580 <tjdrkr2580@naver.com>
2023-02-02 11:01:06 +0900

    ADD | form

--- fe/src/pages/index.tsx
+++ fe/src/pages/index.tsx
@@ -1,8 +1,77 @@
 import { NextPage } from 'next'
+import { useState } from 'react'
+import { useRouter } from 'next/router'
+import axios from 'axios'
+import * as Yup from 'yup'
 import React from 'react'
+import { IconDatabase } from '@tabler/icons'
+import { ShieldCheckIcon } from '@heroicons/react/solid'
+import { ExclamationCircleIcon } from '@heroicons/react/outline'
+import {
+  Anchor,
+  TextInput,
+  Button,
+  Group,
+  PasswordInput,
+  Alert,
+} from '@mantine/core'
+import { useForm, yupResolver } from '@mantine/form'
+import { AuthForm } from 'types'
+import { Layout } from '@/components/Layout'
+
+const schema = Yup.object().shape({
+  email: Yup.string().email('Invalid email').required('No email provided'),
+  password: Yup.string()
+    .required('No Password provided')
+    .min(6, 'Password shoud be min 5 chars'),
+})
 
 const Home: NextPage = () => {
-  return <div>Home</div>
+  const router = useRouter()
+  const [isRegister, setIsRegister] = useState(false)
+  const [error, setError] = useState('')
+  const form = useForm<AuthForm>({
+    validate: yupResolver(schema),
+    initialValues: {
+      email: '',
+      password: '',
+    },
+  })
+  const handleSubmit = async () => {
+    try {
+      if (isRegister) {
+        await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/auth/signup`, {
+          email: form.values.email,
+          password: form.values.password,
+        })
+      }
+      await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/auth/login`, {
+        email: form.values.email,
+        password: form.values.password,
+      })
+      form.reset()
+      router.push('/dashboard')
+    } catch (e: any) {
+      setError(e.response.data.message)
+    }
+  }
+  return (
+    <Layout title="Auth">
+      <ShieldCheckIcon className="h-16 w-16 text-blue-500">
+        {error && (
+          <Alert
+            my="md"
+            variant="filled"
+            icon={<ExclamationCircleIcon />}
+            title="Authorization Error"
+            color="red"
+            radius="md"
+            children={undefined}
+          />
+        )}
+      </ShieldCheckIcon>
+    </Layout>
+  )
 }
 
 export default Home
